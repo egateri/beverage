38d1e54b501caee1953016d561f5d07186f68950
Eliud Gateri <egateri@gmail.com>
2023-02-28 18:53:29 +0300

    Add script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let choice = prompt("Will you take  Tea or Coffe?").toLowerCase();
+if (choice == "tea") {
+  document.getElementById("demo").innerHTML = "You chose Tea";
+} else if (choice == "coffee") {
+  document.getElementById("demo").innerHTML = "You chose Cofee";
+} else {
+  document.getElementById("demo").innerHTML =
+    "Please chose either Tea or Cofee";
+}
