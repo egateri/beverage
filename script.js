c989c5a7ed542630a0f10f9426a715056f66fa9f
Eliud Gateri <123971625+egateri@users.noreply.github.com>
2023-03-04 08:41:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let choice = prompt("Will you take  Tea or Coffe?");
 choice = choice.toLowerCase();
-if (choice == "tea") {
+if (choice === "tea") {
   document.getElementById("demo").innerHTML = "You chose Tea";
-} else if (choice == "coffee") {
+} else if (choice === "coffee") {
   document.getElementById("demo").innerHTML = "You chose Cofee";
 } else {
   document.getElementById("demo").innerHTML =
