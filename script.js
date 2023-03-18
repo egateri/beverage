43514a68be4578e3e758e87fca560a2b0ff71cb5
Eliud Gateri <egateri@gmail.com>
2023-03-18 16:20:45 +0300

    Fix display bug

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let choice = prompt("Will you take  Tea or Coffe?").toLowerCase();
-if (choice == "tea") {
-  document.getElementById("demo").innerHTML = "You chose Tea";
-} else if (choice == "coffee") {
-  document.getElementById("demo").innerHTML = "You chose Cofee";
-} else {
-  document.getElementById("demo").innerHTML =
-    "Please chose either Tea or Cofee";
-}
+const main = () => {
+  let choice = null;
+
+  choice = prompt("Will you take  Tea or Coffe?");
+
+  choice = choice.toLocaleLowerCase();
+
+  if (choice === "tea") {
+    document.getElementById("demo").innerHTML = "You chose Tea";
+    document.getElementById("demo").style.color = "green";
+    //alert("Tea");
+  } else if (choice === "coffee") {
+    document.getElementById("demo").innerHTML = "You chose Cofee";
+    document.getElementById("demo").style.color = "green";
+    //alert("coffe");
+  } else {
+    document.getElementById("demo").innerHTML =
+      "Please chose either Tea or Cofee";
+      document.getElementById("demo").style.color = "red";
+    //alert("Error: Choose tea or coffee");
+  }
+};
