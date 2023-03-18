b5f3b8a17dd54ea92e8028f55eee1cb2846a7859
Eliud Gateri <egateri@gmail.com>
2023-03-18 17:19:38 +0300

    Auto clear the on event field

--- script.js
+++ script.js
@@ -8,15 +8,23 @@ const main = () => {
   if (choice === "tea") {
     document.getElementById("demo").innerHTML = "You chose Tea";
     document.getElementById("demo").style.color = "green";
+    document.getElementById("button").style.display = 'none';
+    document.getElementById("form").style.display = 'block';
+    
     //alert("Tea");
   } else if (choice === "coffee") {
     document.getElementById("demo").innerHTML = "You chose Cofee";
     document.getElementById("demo").style.color = "green";
+    document.getElementById("button").style.display = 'none'
+    document.getElementById("form").style.display = 'block';
     //alert("coffe");
   } else {
     document.getElementById("demo").innerHTML =
       "Please chose either Tea or Cofee";
       document.getElementById("demo").style.color = "red";
-    //alert("Error: Choose tea or coffee");
+      document.getElementById("button").style.display = 'none'
+      document.getElementById("form").style.display = 'block';
+      
+    // alert("Error: Choose tea or coffee");
   }
 };
